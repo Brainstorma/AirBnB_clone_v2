66560783d007b3211ab4fcc5137c45051e4f1d93
Brainstorma <113864294+Brainstorma@users.noreply.github.com>
2023-04-19 01:27:41 +0000

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
-from models.city import City
 import os
 
+from models.city import City
+from tests.test_models.test_base_model import TestBasemodel
 
-class test_City(test_basemodel):
-    """ tests for city """
 
+class TestCity(TestBasemodel):
+    """Represents the tests for the City model."""
     def __init__(self, *args, **kwargs):
-        """ init the test class"""
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ testing state_id type """
+        """Tests the type of state_id."""
         new = self.value()
-        self.assertEqual(type(new.state_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(
+            type(new.state_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_name(self):
-        """ testing name type"""
+        """Tests the type of name."""
         new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
