8b9c09e8a2c56eadc60e90c434a0f60983365ce4
Brainstorma <113864294+Brainstorma@users.noreply.github.com>
2023-04-19 01:24:32 +0000

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
 import os
 
+from tests.test_models.test_base_model import TestBasemodel
+from models.review import Review
 
-class test_review(test_basemodel):
-    """ review test class"""
 
+class TestReview(TestBasemodel):
+    """Represents the tests for the Review model."""
     def __init__(self, *args, **kwargs):
-        """ review class init"""
+        """Initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ testing review place_id attr"""
+        """Tests the type of place_id."""
         new = self.value()
-        self.assertEqual(type(new.place_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(
+            type(new.place_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_user_id(self):
-        """ testing review user_id attr"""
+        """Tests the type of user_id."""
         new = self.value()
-        self.assertEqual(type(new.user_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(
+            type(new.user_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_text(self):
-        """ testing review text attr"""
+        """Tests the type of text."""
         new = self.value()
-        self.assertEqual(type(new.text), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        self.assertEqual(
+            type(new.text),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
