0111f338f1bc0b6c57f1b86823fc69651b189354
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-06-05 22:36:28 +0000

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  1 11:15:54 2020
-
-@author: Robinson Montes
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 app = Flask(__name__)
